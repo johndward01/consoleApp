b521fc1c80ec23a060c0e519ab60702af836c5e8
jdw <johndward01@gmail.com>
2021-12-06 09:35:00 -0600

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x;
+            double y;
+            bool isComplete;
+            char mI;
+            string firstName;
+            decimal balance;
+
+            x = 10;
+            y = 1.99;
+            isComplete = true;
+            mI = 'a';
+            firstName = "John";
+            balance = 10.9834798374893748937489M;
+
+
+
+            string lastName = "Smith";
+
+
+        }
+
+       
+    }
+}
